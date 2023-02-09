801b6415eda29fb2d2b7088d85179dabb2f0547e
Genius <genius21513@gmail.com>
2023-02-10 03:07:54 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
+  // assetPrefix: isProd ? 'https://cdn.mydomain.com' : undefined,
+  basePath: isProd ? '/metakrew' : undefined,  
 }
 
 module.exports = nextConfig
